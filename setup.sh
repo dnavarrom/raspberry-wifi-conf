52b3e26aa1bac8e80b06fba41960cdd0d6ce1f11
diego.navarro.m@gmail.com <diego.navarro.m@gmail.com>
2020-08-06 20:35:30 -0400

    setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+wget https://nodejs.org/dist/v8.9.0/node-v8.9.0-linux-armv6l.tar.gz
+tar -xzf node-v8.9.0-linux-armv6l.tar.gz
+cd node-v8.9.0-linux-armv6l/
+sudo cp -R * /usr/local/
+cd ..
+sudo npm install bower -g
+
+
